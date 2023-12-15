50c2c164ef08a1134b84af65b067ef4eab31f54c
Alex76456 <70705720+Alex76456@users.noreply.github.com>
2023-12-15 22:24:04 +0300

    little fix

--- constants/commonConstants.js
+++ constants/commonConstants.js
@@ -13,5 +13,5 @@ export const gameConfig = {
   MIN_RANDOM_NUMBER: 0,
   MAX_RANDOM_NUMBER: 100,
   NO_PLAYERS_MESSAGE: "Ожидаем игроков...",
-  GET_RESULT_MESSAGE: getResultMessage,
+  GET_RESULT_MESSAGE: ({ winningNumber, winningMessage }) => getResultMessage({ winningNumber, winningMessage }),
 }
