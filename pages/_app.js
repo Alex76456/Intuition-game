a937a94070a4505ee181a0f37f4526218ba0febc
Aleksey Logvenkin <alogvenkin@yandex.ru>
2023-12-17 14:22:31 +0300

    fix favicon display

--- pages/_app.js
+++ pages/_app.js
@@ -10,19 +10,19 @@ export default function MyApp({ Component, pageProps }) {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/public/favicon_io/apple-touch-icon.png"
+          href="/favicon_io/apple-touch-icon.png"
         />
         <link
-          rel="icon"
+          rel="shortcut icon"
           type="image/png"
           sizes="32x32"
-          href="/public/favicon_io/favicon-32x32.png"
+          href="/favicon_io/favicon-32x32.png"
         />
         <link
-          rel="icon"
+          rel="shortcut icon"
           type="image/png"
           sizes="16x16"
-          href="/public/favicon_io/favicon-16x16.png"
+          href="/favicon_io/favicon-16x16.png"
         />
       </Head>
       <Component {...pageProps} />
