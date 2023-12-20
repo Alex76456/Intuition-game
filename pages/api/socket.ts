a872ebb5f8d5f592c07aca9aff9a2933c73f2050
Alex76456 <70705720+Alex76456@users.noreply.github.com>
2023-12-20 18:59:23 +0300

    add pretier config

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,105 +1,111 @@
-import { gameConfig, socketEvents } from "@constants/commonConstants"
-import { ServerStateType } from "allTypes/commonTypes"
-import { getRandomBotMessage, getRandomIntInRange, getUpdatedStatistic, getWinningMessage } from "@utils/commonUtils"
-import { Server } from "socket.io"
+import { gameConfig, socketEvents } from '@constants/commonConstants'
+import { ServerStateType } from 'allTypes/commonTypes'
+import {
+	getRandomBotMessage,
+	getRandomIntInRange,
+	getUpdatedStatistic,
+	getWinningMessage,
+} from '@utils/commonUtils'
+import { Server } from 'socket.io'
 
 const serverState: ServerStateType = {
-  messages: [],
-  nextResultDate: Date.now() + gameConfig.GAME_DURATION,
-  statistic: {},
+	messages: [],
+	nextResultDate: Date.now() + gameConfig.GAME_DURATION,
+	statistic: {},
 }
 
 export default function SocketHandler(req, res) {
-  if (res.socket.server.io) {
-    console.log("Already set up")
-    res.end()
-    return
-  }
+	if (res.socket.server.io) {
+		console.log('Already set up')
+		res.end()
+		return
+	}
 
-  const io = new Server(res.socket.server)
-  res.socket.server.io = io
+	const io = new Server(res.socket.server)
+	res.socket.server.io = io
 
-  io.on("connection", (socket) => {
-    // приветствуем нового юзера
-    io.to(socket.id).emit(socketEvents.RECEIVE_MESSAGE, {
-      username: gameConfig.SERVER_NAME,
-      message: gameConfig.GREETING_MESSAGE,
-    })
+	io.on('connection', (socket) => {
+		// приветствуем нового юзера
+		io.to(socket.id).emit(socketEvents.RECEIVE_MESSAGE, {
+			username: gameConfig.SERVER_NAME,
+			message: gameConfig.GREETING_MESSAGE,
+		})
 
-    io.to(socket.id).emit(socketEvents.STATISTIC_MESSAGE, serverState.statistic)
+		io.to(socket.id).emit(socketEvents.STATISTIC_MESSAGE, serverState.statistic)
 
-    // обработка новых сообщений от всех юзеров
-    socket.on(socketEvents.SEND_MESSAGE, (message) => {
-      serverState.messages = [...serverState.messages, message]
-      io.emit(socketEvents.RECEIVE_MESSAGE, message)
+		// обработка новых сообщений от всех юзеров
+		socket.on(socketEvents.SEND_MESSAGE, (message) => {
+			serverState.messages = [...serverState.messages, message]
+			io.emit(socketEvents.RECEIVE_MESSAGE, message)
 
-      io.to(socket.id).emit(socketEvents.RECEIVE_MESSAGE, {
-        username: gameConfig.INFO_MESSAGE_NAME,
-        message: gameConfig.GET_REMAINING_SECONDS({
-          nextResultDate: serverState.nextResultDate,
-        }),
-      })
-    })
-  })
+			io.to(socket.id).emit(socketEvents.RECEIVE_MESSAGE, {
+				username: gameConfig.INFO_MESSAGE_NAME,
+				message: gameConfig.GET_REMAINING_SECONDS({
+					nextResultDate: serverState.nextResultDate,
+				}),
+			})
+		})
+	})
 
-  // отправка сообщений ботами
-  const emitBotMessage = () => {
-    const nextBotMessageDuration = getRandomIntInRange(gameConfig.FREQUENCY_BOT_MESSAGES) * 1000
-    const newBotMessage = getRandomBotMessage()
+	// отправка сообщений ботами
+	const emitBotMessage = () => {
+		const nextBotMessageDuration =
+			getRandomIntInRange(gameConfig.FREQUENCY_BOT_MESSAGES) * 1000
+		const newBotMessage = getRandomBotMessage()
 
-    setTimeout(() => {
-      serverState.messages = [...serverState.messages, newBotMessage]
-      io.emit(socketEvents.RECEIVE_MESSAGE, newBotMessage)
+		setTimeout(() => {
+			serverState.messages = [...serverState.messages, newBotMessage]
+			io.emit(socketEvents.RECEIVE_MESSAGE, newBotMessage)
 
-      emitBotMessage()
-    }, nextBotMessageDuration)
-  }
-  emitBotMessage()
+			emitBotMessage()
+		}, nextBotMessageDuration)
+	}
+	emitBotMessage()
 
-  // логика цикла игры
-  setInterval(() => {
-    // сообщение "число загадо!"
-    setTimeout(() => {
-      io.emit(socketEvents.RECEIVE_MESSAGE, {
-        username: gameConfig.SERVER_NAME,
-        message: gameConfig.WINNING_NUMBER_CREATED_MESSAGE,
-      })
-    }, 1 * 1000)
+	// логика цикла игры
+	setInterval(() => {
+		// сообщение "число загадо!"
+		setTimeout(() => {
+			io.emit(socketEvents.RECEIVE_MESSAGE, {
+				username: gameConfig.SERVER_NAME,
+				message: gameConfig.WINNING_NUMBER_CREATED_MESSAGE,
+			})
+		}, 1 * 1000)
 
-    if (!serverState.messages.length) {
-      io.emit(socketEvents.RECEIVE_MESSAGE, {
-        username: gameConfig.SERVER_NAME,
-        message: gameConfig.NO_PLAYERS_MESSAGE,
-      })
-      return
-    }
+		if (!serverState.messages.length) {
+			io.emit(socketEvents.RECEIVE_MESSAGE, {
+				username: gameConfig.SERVER_NAME,
+				message: gameConfig.NO_PLAYERS_MESSAGE,
+			})
+			return
+		}
 
-    const winningNumber = getRandomIntInRange({
-      min: gameConfig.MIN_RANDOM_NUMBER,
-      max: gameConfig.MAX_RANDOM_NUMBER,
-    })
-    const winningMessage = getWinningMessage({
-      winningNumber,
-      messages: serverState.messages,
-    })
+		const winningNumber = getRandomIntInRange({
+			min: gameConfig.MIN_RANDOM_NUMBER,
+			max: gameConfig.MAX_RANDOM_NUMBER,
+		})
+		const winningMessage = getWinningMessage({
+			winningNumber,
+			messages: serverState.messages,
+		})
 
-    io.emit(socketEvents.RECEIVE_MESSAGE, {
-      username: gameConfig.SERVER_NAME,
-      message: gameConfig.GET_RESULT_MESSAGE({ winningNumber, winningMessage }),
-    })
+		io.emit(socketEvents.RECEIVE_MESSAGE, {
+			username: gameConfig.SERVER_NAME,
+			message: gameConfig.GET_RESULT_MESSAGE({ winningNumber, winningMessage }),
+		})
 
-    serverState.statistic = getUpdatedStatistic({
-      winningNumber,
-      startStatistic: serverState.statistic,
-      messages: serverState.messages,
-      winningMessage,
-    })
+		serverState.statistic = getUpdatedStatistic({
+			winningNumber,
+			startStatistic: serverState.statistic,
+			messages: serverState.messages,
+			winningMessage,
+		})
 
-    io.emit(socketEvents.STATISTIC_MESSAGE, serverState.statistic)
-    serverState.messages = []
-    serverState.nextResultDate = Date.now() + gameConfig.GAME_DURATION
-  }, gameConfig.GAME_DURATION)
+		io.emit(socketEvents.STATISTIC_MESSAGE, serverState.statistic)
+		serverState.messages = []
+		serverState.nextResultDate = Date.now() + gameConfig.GAME_DURATION
+	}, gameConfig.GAME_DURATION)
 
-  console.log("Setting up socket")
-  res.end()
+	console.log('Setting up socket')
+	res.end()
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,135 +1,164 @@
-import { RULES, SOCKET_URL, socketEvents } from "@constants/commonConstants"
-import { CommonStatisticType, MessageType } from "allTypes/commonTypes"
-import { getRandomIntInRange } from "@utils/commonUtils"
-import React, { useEffect, useRef, useState } from "react"
-import io from "socket.io-client"
+import { RULES, SOCKET_URL, socketEvents } from '@constants/commonConstants'
+import { CommonStatisticType, MessageType } from 'allTypes/commonTypes'
+import { getRandomIntInRange } from '@utils/commonUtils'
+import React, { useEffect, useRef, useState } from 'react'
+import io from 'socket.io-client'
 
 const Home = () => {
-  const socket = useRef(null)
+	const socket = useRef(null)
 
-  const [message, setMessage] = useState<string>("")
-  const [username, setUsername] = useState<string>(`IntuiUser(${getRandomIntInRange({ min: 1, max: 999 })})`)
-  const [allMessages, setAllMessages] = useState<MessageType[]>([])
-  const [statistic, setStatistic] = useState<CommonStatisticType>({})
+	const [message, setMessage] = useState<string>('')
+	const [username, setUsername] = useState<string>(
+		`IntuiUser(${getRandomIntInRange({ min: 1, max: 999 })})`
+	)
+	const [allMessages, setAllMessages] = useState<MessageType[]>([])
+	const [statistic, setStatistic] = useState<CommonStatisticType>({})
 
-  const accuracyLeaders = Object.entries(statistic).sort((a, b) => b[1].averageAccuracy - a[1].averageAccuracy)
-  const winsLeaders = Object.entries(statistic).sort((a, b) => b[1].wins - a[1].wins)
-  const gamesPlayedLeaders = Object.entries(statistic).sort((a, b) => b[1].gamesPlayed - a[1].gamesPlayed)
-  const numbersSuggestedLeaders = Object.entries(statistic).sort(
-    (a, b) => b[1].numbersSuggested - a[1].numbersSuggested,
-  )
+	const accuracyLeaders = Object.entries(statistic).sort(
+		(a, b) => b[1].averageAccuracy - a[1].averageAccuracy
+	)
+	const winsLeaders = Object.entries(statistic).sort(
+		(a, b) => b[1].wins - a[1].wins
+	)
+	const gamesPlayedLeaders = Object.entries(statistic).sort(
+		(a, b) => b[1].gamesPlayed - a[1].gamesPlayed
+	)
+	const numbersSuggestedLeaders = Object.entries(statistic).sort(
+		(a, b) => b[1].numbersSuggested - a[1].numbersSuggested
+	)
 
-  useEffect(() => {
-    socketInitializer()
-    return () => {
-      socket.current.disconnect()
-    }
-  }, [])
+	useEffect(() => {
+		socketInitializer()
+		return () => {
+			socket.current.disconnect()
+		}
+	}, [])
 
-  async function socketInitializer() {
-    await fetch(SOCKET_URL)
-    socket.current = io()
-    socket.current.on(socketEvents.RECEIVE_MESSAGE, (data: MessageType) => {
-      setAllMessages((pre) => [...pre, data])
-    })
+	async function socketInitializer() {
+		await fetch(SOCKET_URL)
+		socket.current = io()
+		socket.current.on(socketEvents.RECEIVE_MESSAGE, (data: MessageType) => {
+			setAllMessages((pre) => [...pre, data])
+		})
 
-    socket.current.on(socketEvents.STATISTIC_MESSAGE, (data: CommonStatisticType) => {
-      setStatistic(data)
-    })
-  }
+		socket.current.on(
+			socketEvents.STATISTIC_MESSAGE,
+			(data: CommonStatisticType) => {
+				setStatistic(data)
+			}
+		)
+	}
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    console.log("emitted")
-    socket.current.emit(socketEvents.SEND_MESSAGE, {
-      username,
-      message,
-    })
-    setMessage("")
-  }
+	function handleSubmit(e) {
+		e.preventDefault()
+		console.log('emitted')
+		socket.current.emit(socketEvents.SEND_MESSAGE, {
+			username,
+			message,
+		})
+		setMessage('')
+	}
 
-  return (
-    <div className="main">
-      <div className="rules">
-        <h2>Правила</h2>
-        <ul className="rulesList">
-          {RULES.map((rule, index) => (
-            <li key={index} className="listItem">
-              {rule}
-            </li>
-          ))}
-        </ul>
-      </div>
+	return (
+		<div className='main'>
+			<div className='rules'>
+				<h2>Правила</h2>
+				<ul className='rulesList'>
+					{RULES.map((rule, index) => (
+						<li key={index} className='listItem'>
+							{rule}
+						</li>
+					))}
+				</ul>
+			</div>
 
-      <div className="mainWrapper">
-        <div className="mainInner">
-          <div className="name">
-            <h1 className="nameTitle">Great Intuition the Game</h1>
-          </div>
+			<div className='mainWrapper'>
+				<div className='mainInner'>
+					<div className='name'>
+						<h1 className='nameTitle'>Great Intuition the Game</h1>
+					</div>
 
-          <div className="username">
-            <h2 className="usernameSubtitle">{"Ваш ник (можно ввести свой):"}</h2>
-            <input className="input" value={username} onChange={(e) => setUsername(e.target.value)} />
-          </div>
+					<div className='username'>
+						<h2 className='usernameSubtitle'>
+							{'Ваш ник (можно ввести свой):'}
+						</h2>
+						<input
+							className='input'
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+					</div>
 
-          <div className="window">
-            <ul className="windowChat">
-              {allMessages.map(({ username, message }, index) => (
-                <li key={index} className="message">
-                  {username}: {message}
-                </li>
-              ))}
-            </ul>
+					<div className='window'>
+						<ul className='windowChat'>
+							{allMessages.map(({ username, message }, index) => (
+								<li key={index} className='message'>
+									{username}: {message}
+								</li>
+							))}
+						</ul>
 
-            <form className="form" onSubmit={handleSubmit}>
-              <input
-                className="inputMessage"
-                disabled={!username}
-                name="message"
-                placeholder="введите своё число"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                autoComplete={"off"}
-                type="number"
-                size={3}
-              />
-              <button className="submitButton" type="submit" disabled={!username || !message}>
-                Send
-              </button>
-            </form>
-          </div>
-        </div>
-      </div>
-      <div className="leaderboard">
-        <h2>Таблица лидеров по точности</h2>
-        <ul className="leaderboardList">
-          {accuracyLeaders.map((user, index) => (
-            <li key={user[0]}>{`${index + 1}. ${user[0]} (Ср. точность: ${user[1].averageAccuracy}%)`}</li>
-          ))}
-        </ul>
-        <h2>Таблица лидеров по количеству побед</h2>
-        <ul>
-          {winsLeaders.map((user, index) => (
-            <li key={user[0]}>{`${index + 1}. ${user[0]} (Побед: ${user[1].wins})`}</li>
-          ))}{" "}
-        </ul>
+						<form className='form' onSubmit={handleSubmit}>
+							<input
+								className='inputMessage'
+								disabled={!username}
+								name='message'
+								placeholder='введите своё число'
+								value={message}
+								onChange={(e) => setMessage(e.target.value)}
+								autoComplete={'off'}
+								type='number'
+								size={3}
+							/>
+							<button
+								className='submitButton'
+								type='submit'
+								disabled={!username || !message}
+							>
+								Send
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div className='leaderboard'>
+				<h2>Таблица лидеров по точности</h2>
+				<ul className='leaderboardList'>
+					{accuracyLeaders.map((user, index) => (
+						<li key={user[0]}>{`${index + 1}. ${user[0]} (Ср. точность: ${
+							user[1].averageAccuracy
+						}%)`}</li>
+					))}
+				</ul>
+				<h2>Таблица лидеров по количеству побед</h2>
+				<ul>
+					{winsLeaders.map((user, index) => (
+						<li key={user[0]}>{`${index + 1}. ${user[0]} (Побед: ${
+							user[1].wins
+						})`}</li>
+					))}{' '}
+				</ul>
 
-        <h2>Таблица лидеров по участию в играх</h2>
-        <ul>
-          {gamesPlayedLeaders.map((user, index) => (
-            <li key={user[0]}>{`${index + 1}. ${user[0]} (Сыграно: ${user[1].gamesPlayed})`}</li>
-          ))}
-        </ul>
+				<h2>Таблица лидеров по участию в играх</h2>
+				<ul>
+					{gamesPlayedLeaders.map((user, index) => (
+						<li key={user[0]}>{`${index + 1}. ${user[0]} (Сыграно: ${
+							user[1].gamesPlayed
+						})`}</li>
+					))}
+				</ul>
 
-        <h2>Таблица лидеров по количеству предложений</h2>
-        <ul>
-          {numbersSuggestedLeaders.map((user, index) => (
-            <li key={user[0]}>{`${index + 1}. ${user[0]} (Предложено: ${user[1].numbersSuggested})`}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  )
+				<h2>Таблица лидеров по количеству предложений</h2>
+				<ul>
+					{numbersSuggestedLeaders.map((user, index) => (
+						<li key={user[0]}>{`${index + 1}. ${user[0]} (Предложено: ${
+							user[1].numbersSuggested
+						})`}</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	)
 }
 
 export default Home
